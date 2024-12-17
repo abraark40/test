94f9055f1dce99009feba27ffed9354ab22397a9
abraark40 <132936920+abraark40@users.noreply.github.com>
2024-12-17 15:42:02 +0400

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,12 @@
+import unittest, git, argparse
+
+
+class TestGitDummy(unittest.TestCase):
+    def test_git_dummy(self):
+        """Test git dummy."""
+
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
